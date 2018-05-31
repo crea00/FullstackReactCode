df036a91b8aa760ee82c5055096a27b98d3ff198
crea <654321su@gmail.com>
2018-05-31 10:31:15 +0900

    #036 Saving Model Instances

--- server/models/User.js
+++ server/models/User.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-    googleId: String,
-    name: String
+    googleId: String
 });
 
 mongoose.model('users', userSchema);
